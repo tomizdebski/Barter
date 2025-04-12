8188e6ef6078b616618360c489a72c03b3282d2d
Tomasz Izdebski <tomizdebski@email.com>
2025-04-12 20:15:27 +0200

    features/end-create-lesson-succesfully

--- barter-api/src/lessons/lessons.service.ts
+++ barter-api/src/lessons/lessons.service.ts
@@ -5,17 +5,22 @@ import { PrismaService } from 'prisma/prisma.service';
 export class LessonsService {
   constructor(private prisma: PrismaService) {}
 
-  async create(dto: any, photo?: Express.Multer.File, video?: Express.Multer.File) {
+  async create(
+    dto: any,
+    photo?: Express.Multer.File,
+    video?: Express.Multer.File,
+  ) {
     return this.prisma.lessons.create({
       data: {
         name: dto.name,
         content: dto.content,
         categoryId: parseInt(dto.categoryId),
-        photo: photo?.path || null,
-        video: video?.path || null,
-        instructorId: 2, // przykładowo – pobierz z auth contextu później
+        photo: photo ? `uploads/${photo.filename}` : null,
+        video: video ? `uploads/${video.filename}` : null,
+        instructorId: 2, // zamień na userId z tokena, jeśli już masz auth
       },
     });
   }
 }
+
 
