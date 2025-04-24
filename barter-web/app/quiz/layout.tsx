6395b8a3b1fd65e85da0228a370b97a72c9cf03f
Tomasz Izdebski <tomizdebski@email.com>
2025-04-24 21:29:56 +0200

    features/sorting_working

--- barter-web/app/quiz/layout.tsx
+++ barter-web/app/quiz/layout.tsx
@@ -1,14 +1,10 @@
 // app/layout.tsx lub app/quiz/layout.tsx
-import { QuizProvider } from '@/contexts/QuizContext';
+import { QuizProvider } from "@/contexts/QuizContext";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <QuizProvider>
-          {children}
-        </QuizProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <QuizProvider>{children}</QuizProvider>;
 }
