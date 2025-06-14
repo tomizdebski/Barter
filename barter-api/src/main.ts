c2e119a624b67302576e053808bae98d2f15ce68
tomizdebski <tomizdebski@gmail.com>
2025-06-14 15:00:42 +0200

    fix/cors

--- barter-api/src/main.ts
+++ barter-api/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://172.31.9.218:3000', `${process.env.CORS_ORIGIN}`, 'http://localhost:3000'],
+    origin: ['http://172.31.9.218:3000', `${process.env.CORS_ORIGIN}`, 'http://localhost:3000', 'https://barter-roz3.onrender.com'],
     credentials: true,
   });
   app.useGlobalPipes(
